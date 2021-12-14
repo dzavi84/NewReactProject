3463b9cef7f1746dd73d144af33da93e43a9a223
dzavi84 <78727003+dzavi84@users.noreply.github.com>
2021-12-14 23:01:16 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 if (module.hot) {
   module.hot.accept();
@@ -10,13 +11,18 @@ if (module.hot) {
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <ApprovalCard>
+        <CommentDetail  author='Sam' timeAgo='Today at 4PM' blog='Nice Blog Post' avatar={faker.image.image()}/>
+      </ApprovalCard>
+      <ApprovalCard>        <CommentDetail author='Jane' timeAgo='Today at 9PM' blog='Great Blog Post' avatar={faker.image.image()}/>
+</ApprovalCard>
+<ApprovalCard>        <CommentDetail  author='John' timeAgo='Today at 6AM' blog='Awsome Blog Post' avatar={faker.image.image()}/>
+</ApprovalCard>
+
+      
     </div>
   );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
+ 
